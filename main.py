3d5d324a5cba1509ddfd1e128647e5eb01afc330
quandangv <quandangv@gmail.com>
2021-11-05 17:49:57 +0700

    Add batch subcommand

--- main.py
+++ main.py
@@ -15,28 +15,32 @@ compress_args = argparse.ArgumentParser(add_help=False)
 compress_args.add_argument('--detail-level', '-d', type=float, default=1, help="The level of detail in the image archive. Must be positive, default is 1")
 compress_args.add_argument('--quality', '-q', type=float, default=0.7, help="The compression quality (from 0 to 1) of the image archive")
 
+simple_io = argparse.ArgumentParser(add_help=False)
+simple_io.add_argument('paths', metavar='input', action='append', help="Path to input file")
+simple_io.add_argument('paths', metavar='output', action='append', nargs='?', help='Path to save the output file. Defaults to saving next to the input')
+
 parser = argparse.ArgumentParser(description="Image archiver using an AI upscale algorithm.")
 parser.add_argument('--debug', action='store_true', help="Saves intermediary results for debug purpose")
-subparsers = parser.add_subparsers(description="Specifies an action to do", required=True, dest='action')
+subparsers = parser.add_subparsers(title='actions', description="Specifies an action to do", required=True, dest='action')
 
-parent_io = argparse.ArgumentParser(add_help=False)
-parent_io.add_argument('input', help="Path to input file")
-parent_io.add_argument('output', nargs='?', help='Path to save the output file. Defaults to saving next to the input')
-
-parser_compress = subparsers.add_parser('compress', help="Compresses an image", aliases=['c'], parents=[parent_io, compress_args])
-parser_extract = subparsers.add_parser('extract', help="Extracts image from an archive", aliases=['e'], parents=[parent_io])
+parser_batch = subparsers.add_parser('batch', help="Processes a list of inputs", aliases=['b'], parents=[compress_args])
+parser_compress = subparsers.add_parser('compress', help="Compresses an image", aliases=['c'], parents=[simple_io, compress_args])
+parser_extract = subparsers.add_parser('extract', help="Extracts image from an archive", aliases=['e'], parents=[simple_io])
 parser_test = subparsers.add_parser('test', help="Compresses an image then extract to view the result", aliases=['t'], parents=[compress_args])
 
 parser_compress.set_defaults(action='compress')
 parser_extract.set_defaults(action='extract')
 parser_test.set_defaults(action='test')
 
-parser_test.add_argument('input', help='Path to input file')
-parser_test.add_argument('archive_output', nargs='?', help='Path to save the output archive. Defaults to saving next to the input')
-parser_test.add_argument('image_output', nargs='?', help='Path to save the output image. Defaults to saving next to the output archive')
+parser_test.add_argument('paths', metavar='input', action='append', help="Path to input file")
+parser_test.add_argument('paths', metavar='archive_output', action='append', nargs='?', help="Path to save the output archive. Defaults to saving next to the input")
+parser_test.add_argument('paths', metavar='image_output', action='append', nargs='?', help="Path to save the output image. Defaults to saving next to the output archive")
+
+parser_batch.add_argument('action', choices=['compress', 'extract', 'test'], help="Action to perform on the inputs")
+parser_batch.add_argument('batch', metavar='inputs', nargs='+', help="The list of inputs to process")
 
 args = parser.parse_args(sys.argv[1:])
-#print(args)
+print(args)
 assert args.detail_level > 0, f"Invalid detail_level: {args.detail_level}"
 assert args.quality > 0 and args.quality <= 1, f"Invalid quality: {args.quality}"
 
@@ -87,7 +91,7 @@ def compress(input_path, output_path):
 
   debug_save(img, 'pad.png')
 
-  os.makedirs(os.path.dirname(output_path))
+  os.makedirs(os.path.dirname(output_path), exist_ok=True)
   with zipfile.ZipFile(output_path, mode='w') as output:
     jpeg_loss = 100 - args.quality*100
     def save_layer(index, color, alpha):
@@ -99,7 +103,7 @@ def compress(input_path, output_path):
         if jpeg_quality > 95:
           img.save(f, 'PNG', optimize = True)
         else:
-          img.save(f, 'JPEG', quality = jpeg_quality, subsampling = 2, optimize = True)
+          img.save(f, 'JPEG', quality=jpeg_quality, subsampling=2, optimize=True)
       save_image(img, f, jpeg_quality)
       output.write(f.name, arcname=f'layer{index}')
       f.close()
@@ -115,9 +119,9 @@ def compress(input_path, output_path):
               val //= 2
         for bit_idx in range(bits_per_pixel):
           with tempfile.NamedTemporaryFile() as f:
-            sequence[bit_idx].save(f, 'PNG', optimize = True)
+            sequence[bit_idx].save(f, 'PNG', optimize=True)
             output.write(f.name, arcname=f'layer{index}_alpha{bit_idx}')
-      #alpha.save(f, 'PNG', optimize = True)
+      #alpha.save(f, 'PNG', optimize=True)
       #output.write(f.name, arcname=f'layer{index}_alpha')
       save_1_sequence(alpha, alpha_bpp)
       f.close()
@@ -187,11 +191,23 @@ def extract(input_path, output_path):
 def replace_ext(path, new_ext):
   return os.path.splitext(path)[0] + new_ext
 
-if args.action == 'compress':
-  compress(args.input, args.output or replace_ext(args.input, '.zip'))
-elif args.action == 'extract':
-  extract(args.input, args.output or replace_ext(args.input, '.png'))
-elif args.action == 'test':
-  args.archive_output = args.archive_output or replace_ext(args.input, '.zip')
-  compress(args.input, args.archive_output)
-  extract(args.archive_output, args.image_output or replace_ext(args.archive_output, '.png'))
+def action_compress(input, output=None):
+  compress(input, output or replace_ext(input, '.zip'))
+
+def action_extract(input, output=None):
+  extract(input, output or replace_ext(input, '.png'))
+
+def action_test(input, archive_output=None, image_output=None):
+  archive_output = archive_output or replace_ext(input, '.zip')
+  if not image_output:
+    image_output = image_output or replace_ext(archive_output, '.png')
+    if image_output == input:
+      image_output = replace_ext(image_output, '_out.png')
+  compress(input, archive_output)
+  extract(archive_output, image_output)
+
+if args.batch:
+  for input in args.batch:
+    globals()['action_' + args.action](input)
+else:
+  globals()['action_' + args.action](*args.paths)
